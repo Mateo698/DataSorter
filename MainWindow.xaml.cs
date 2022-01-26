7d75917ef55c06eac40f191d96088e970a713e28
Mateo <mateora73@gmail.com>
2022-01-26 14:02:27 -0500

    CSV to DataGrid working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +27,88 @@ namespace DataSorter
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(comboBox.SelectedItem != null)
+            {
+
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == true)
+            {
+                dataTable.ItemsSource = bindData(openFileDialog.FileName);
+            }
+        }
+
+        public List<Municipality> filteredData(string path)
+        {
+            var lines = File.ReadAllLines(path);
+
+            var data = from l in lines.Skip(1)
+                       let split = l.Split(',')
+                       if (true)
+            {
+                select new Municipality
+                {
+                    depCode = split[0],
+                    munCode = split[1],
+                    depName = split[2],
+                    munName = split[3],
+                    type = split[4]
+                };
+            };
+                       
+            return data.ToList();
+        }
+
+        public List<Municipality> bindData(string path)
+        {
+            var lines = File.ReadAllLines(path);
+
+            List<String> deps = new List<String>();
+
+            foreach (var line in lines.Skip(1))
+            {
+                string[] parts = line.Split(',');
+                if(!deps.Contains(parts[2]))
+                {
+                    deps.Add(parts[2]);
+                };
+            }
+
+            comboBox.ItemsSource = deps;
+
+            var data = from l in lines.Skip(1)
+                       let split = l.Split(',')
+                        select new Municipality
+                        {
+                            depCode = split[0],
+                            munCode = split[1],
+                            depName = split[2],
+                            munName = split[3],
+                            type = split[4]
+                        };
+            return data.ToList();
+        }
+    }
+
+    public class Municipality
+    {
+        public string depCode { get; set; }
+        public string munCode { get; set; }    
+        public string depName { get; set; }
+        public string munName { get; set; }
+        public string type { get; set; }
+
     }
 }
